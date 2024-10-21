d753e40f7caeda7d1a3abd52bd0c7eb54f853b6f
andrey-sysoev <andrey.sysoev@stud.hshl.de>
2024-10-21 21:40:52 +0300

    Update TrafficLight.cpp

--- TrafficLight.cpp
+++ TrafficLight.cpp
@@ -1,42 +1,62 @@
+#define RED 100
+#define YELLOW 200
+#define GREEN 300
+
 class TrafficLight {
   private:
     int led_red;
     int led_yellow;
     int led_green;
+    int currentState=RED;
+    
   public:
     TrafficLight(int red, int yel, int gre) {
       led_red = red;
       led_yellow = yel;
       led_green = gre;
     }
+
     void setup() {
       pinMode(led_red, OUTPUT);
       pinMode(led_yellow, OUTPUT);
       pinMode(led_green, OUTPUT);
-     
     }
-    void switchingLights() {
-      digitalWrite(led_red, HIGH);
-      digitalWrite(led_yellow, LOW);
-      digitalWrite(led_green, LOW);
-      delay(20000);
 
-      digitalWrite(led_red, LOW);
-      digitalWrite(led_yellow, HIGH);
-      digitalWrite(led_green, LOW);
-      delay(5000);  
-
-      digitalWrite(led_red, LOW);
-      digitalWrite(led_yellow, LOW);
-      digitalWrite(led_green, HIGH);
-      delay(20000); 
+    void switchLights() {
+      switch (currentState) {
+        case RED:
+          digitalWrite(led_red, HIGH);
+          digitalWrite(led_yellow, LOW);
+          digitalWrite(led_green, LOW);
+          delay(20000);  
+          currentState = YELLOW;  
+          break;
+          
+        case YELLOW:
+          digitalWrite(led_red, LOW);
+          digitalWrite(led_yellow, LOW);
+          digitalWrite(led_green, HIGH);
+          delay(5000); 
+          currentState = GREEN; 
+          break;
+          
+        case GREEN:
+          digitalWrite(led_red, LOW);
+          digitalWrite(led_yellow, HIGH);
+          digitalWrite(led_green, LOW);
+          delay(20000);  
+          currentState = RED;  
+          break;
+      }
     }
 };
-TrafficLight trafficLight(0,1,2);
+
+TrafficLight trafficLight(2, 3, 4);
+
 void setup() {
   trafficLight.setup();
 }
 
 void loop() {
-  trafficLight.switchingLights();
+  trafficLight.switchLights();
 }
